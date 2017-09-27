472a5dfe3c9c2a8a6a4a4ac3cb7f0b5f83dfb6dd
carolinaisaza1 <carito9994@gmail.com>
2017-09-26 22:10:55 -0500

    fechas

--- src/main/java/dominio/Bibliotecario.java
+++ src/main/java/dominio/Bibliotecario.java
@@ -1,5 +1,6 @@
 package dominio;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import dominio.excepcion.PrestamoException;
@@ -11,7 +12,7 @@ public class Bibliotecario {
 	public static final String EL_LIBRO_NO_SE_ENCUENTRA_DISPONIBLE = "El libro no se encuentra disponible";
 	public static final String EL_LIBRO_SOLO_USO_BIBLIOTECA = "los libros palíndromos solo se pueden utilizar en la biblioteca ";
 	public static final int MAXIMO_ISBN = 30;
-	
+
 	private RepositorioLibro repositorioLibro;
 	private RepositorioPrestamo repositorioPrestamo;
 
@@ -27,15 +28,15 @@ public class Bibliotecario {
 			if (esPrestado(isbn)) {
 				throw new PrestamoException(EL_LIBRO_NO_SE_ENCUENTRA_DISPONIBLE);
 			}
-			
+
 			if (esPalindromo(isbn)) {
 				throw new PrestamoException(EL_LIBRO_SOLO_USO_BIBLIOTECA);
 			}
-			if(sumarNumerosISBN(isbn)> MAXIMO_ISBN ){
+			if (sumarNumerosISBN(isbn) > MAXIMO_ISBN) {
 				System.out.println("mayor isbn");
 			}
 			Libro libro = repositorioLibro.obtenerPorIsbn(isbn);
-			Prestamo prestamo = new Prestamo(new Date(),libro,fechaEntrega,nombreUsuario);
+			Prestamo prestamo = new Prestamo(new Date(), libro, fechaEntrega, nombreUsuario);
 			repositorioPrestamo.agregar(prestamo);
 
 		}
@@ -51,16 +52,41 @@ public class Bibliotecario {
 		StringBuilder isbnBuilder = new StringBuilder(isbn);
 		return isbnBuilder.toString().equals(isbnBuilder.reverse().toString());
 	}
-	
-	public int sumarNumerosISBN(String isbn){
+
+	public int sumarNumerosISBN(String isbn) {
 		int suma = 0;
-		char [] caracteres= isbn.toCharArray();
+		char[] caracteres = isbn.toCharArray();
 		for (char c : caracteres) {
-			if(Character.isDigit(c)){
+			if (Character.isDigit(c)) {
 				suma += Integer.parseInt(String.valueOf(c));
 			}
 		}
 		return suma;
+	}
+
+	public static Date calculateDate(Date startDate) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(startDate);
+		// calendar.set(2017, Calendar.SEPTEMBER, 26);
+		Date finalDate = null;
+		int days = 15;
+		while (finalDate == null) {
+			if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
+				int sundays = days / 7;
+				days = days + sundays;
+				calendar.add(Calendar.DAY_OF_MONTH, days);
+				finalDate = calendar.getTime();
+			} else {
+				days--;
+				calendar.add(Calendar.DAY_OF_MONTH, 1);
+			}
+
+		}
+		if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
+			calendar.add(Calendar.DAY_OF_MONTH, 1);
+		}
+		finalDate = calendar.getTime();
+		return finalDate;
 	}
 
 }
